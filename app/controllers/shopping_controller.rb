253e6d0fe4994a4040c3547c457c94f72b2f5292
Sergio Toro <debian.storo@gmail.com>
2013-01-21 17:51:04 -0300

    added method add for shoppping_cart

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -4,4 +4,34 @@ class ShoppingController < ApplicationController
 
 	end
 
+	def add
+		if params[:shopping]	
+
+			if session[:shopping_cart]
+				_cart = Hash.new
+				_cart[:item] = params[:item]
+				_cart[:price] = params[:price]
+				_cart[:quantity] = params[:quantity]
+				_cart[:item_type] = params[:item_type]
+				session[:shopping_cart][session[:shopping_cart].length] = _cart
+			else
+				session[:shopping_cart] = Array.new
+				_cart = Hash.new
+				_cart[:item] = params[:item]
+				_cart[:price] = params[:price]
+				_cart[:quantity] = params[:quantity]
+				_cart[:item_type] = params[:item_type]
+				session[:shopping_cart][0] = _cart
+				
+			end
+
+			redirect_to '/shopping_cart'
+		end
+	end 
+
+
+	def shopping_checkout
+
+	end
+
 end
